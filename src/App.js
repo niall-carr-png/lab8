c4a71838406ac801bb9a612c1318619c8c09fe18
Niall Carr <niall.carr@hotmail.com>
2022-10-03 16:45:43 +0100

    lab2

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { Content } from './components/content';
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom"
+
+class App extends React.Component {
+  render(){
+  return (
+    <Router>
+      <div>
+
+      {/*some comment about my code*/}
+    <div className="App">
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand href="/">Brand link</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/read">Read</Nav.Link>
+            <Nav.Link href="/create">Create</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path='/' element={<Content></Content>} exact></Route>
+        <Route path='/read' element={<Header />}></Route>
+        <Route path='/create' element={<Footer />}></Route>
+      </Routes>
+        {/* <Header></Header>
+        <Content></Content>
+        <Footer></Footer> */}
+    </div>
+      </div>
+    </Router>
+  );
+}
+}
+
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Footer extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>My Footer is another component</h1>
+            </div>
+        )
+    }
+}
